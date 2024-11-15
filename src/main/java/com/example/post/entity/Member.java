8ffc4de414f771cb2a53932ecc39c5ec569c1c0c
haewook Lee <atolow@gmail.com>
2024-11-15 16:39:07 +0900

    level4

--- src/main/java/com/example/post/entity/Member.java
+++ src/main/java/com/example/post/entity/Member.java
@@ -17,7 +17,7 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     public Member() {
